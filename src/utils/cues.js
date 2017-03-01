96d6ac82dda0c2084bf7807578b82ed308f74be6
Evol Greaves <evol@jwplayer.com>
2017-03-01 18:40:44 -0500

    Use line breaks instead of individual cues for combining rows of captions text JW7-3967

--- src/utils/cues.js
+++ src/utils/cues.js
@@ -4,18 +4,16 @@ const Cues = {
 
   newCue: function(track, startTime, endTime, captionScreen) {
     var row;
+    var line;
     var cue;
-    var indenting;
-    var indent;
-    var text;
+    var indenting = true;
+    var indent = 0;
+    var text = captionScreen.getDisplayText();
     var VTTCue = window.VTTCue || window.TextTrackCue;
 
     for (var r=0; r<captionScreen.rows.length; r++)
     {
       row = captionScreen.rows[r];
-      indenting = true;
-      indent = 0;
-      text = '';
 
       if (!row.isEmpty())
       {
@@ -27,13 +25,9 @@ const Cues = {
           }
           else
           {
-            text += row.chars[c].uchar;
             indenting = false;
           }
         }
-        //To be used for cleaning-up orphaned roll-up captions
-        row.cueStartTime = startTime;
-        cue = new VTTCue(startTime, endTime, fixLineBreaks(text.trim()));
 
         if (indent >= 16)
         {
@@ -48,18 +42,22 @@ const Cues = {
         // also, drop line 1 since it's to close to the top
         if (navigator.userAgent.match(/Firefox\//))
         {
-          cue.line = r + 1;
+          line = r + 1;
         }
         else
         {
-          cue.line = (r > 7 ? r - 2 : r + 1);
+          line = (r > 7 ? r - 2 : r + 1);
         }
-        cue.align = 'left';
-        // Clamp the position between 0 and 100 - if out of these bounds, Firefox throws an exception and captions break
-        cue.position = Math.max(0, Math.min(100, 100 * (indent / 32) + (navigator.userAgent.match(/Firefox\//) ? 50 : 0)));
-        track.addCue(cue);
+        break;
       }
     }
+
+    cue = new VTTCue(startTime, endTime, fixLineBreaks(text.trim()));
+    cue.align = 'left';
+    cue.line = line;
+    // Clamp the position between 0 and 100 - if out of these bounds, Firefox throws an exception and captions break
+    cue.position = Math.max(0, Math.min(100, 100 * (indent / 32) + (navigator.userAgent.match(/Firefox\//) ? 50 : 0)));
+    track.addCue(cue);
   }
 
 };
