602c8cda2038ba2eb0e9b0175958d4c84e71c6c5
Evol Greaves <evol@jwplayer.com>
2017-04-12 12:03:04 -0400

    Capture CLOSED-CAPTIONS type from master manifest

--- src/controller/timeline-controller.js
+++ src/controller/timeline-controller.js
@@ -207,16 +207,38 @@ class TimelineController extends EventHandler {
 
       this.tracks.forEach((track, index) => {
         let textTrack;
-        const inUseTrack = inUseTracks[index];
-        // Reuse tracks with the same label, but do not reuse 608/708 tracks
-        if (reuseVttTextTrack(inUseTrack, track)) {
-          textTrack = inUseTrack;
-        } else {
-          textTrack = this.createTextTrack('subtitles', track.name, track.lang);
+      const inUseTrack = inUseTracks[index];
+      // Reuse tracks with the same label, but do not reuse 608/708 tracks
+      if (reuseVttTextTrack(inUseTrack, track)) {
+        textTrack = inUseTrack;
+      } else {
+        textTrack = this.createTextTrack('subtitles', track.name, track.lang);
+      }
+      textTrack.mode = track.default ? 'showing' : 'hidden';
+      this.textTracks.push(textTrack);
+    });
+    }
+
+    if (this.config.enableCEA708Captions && data.captions) {
+      let captionsTrack;
+      let index;
+      let instreamIdMatch;
+
+      for (let i = 0; i < data.captions.length; i++) {
+        captionsTrack = data.captions[i];
+        instreamIdMatch = /(?:CC|SERVICE)([1-2])/.exec(captionsTrack.instreamId);
+
+        if (!instreamIdMatch) {
+          continue;
         }
-        textTrack.mode = track.default ? 'showing' : 'hidden';
-        this.textTracks.push(textTrack);
-      });
+
+        index = instreamIdMatch[1];
+        this.config['captionsTextTrack' + index + 'Label'] = captionsTrack.name;
+
+        if (captionsTrack.lang) { // optional attribute
+          this.config['captionsTextTrack' + index + 'LanguageCode'] = captionsTrack.lang;
+        }
+      }
     }
   }
 
