5a99d1d635ae2aa28fd897a027fadeb4b2600429
DanFerrer <danielferrer212@gmail.com>
2017-10-26 18:13:20 -0400

    refactor utf8ArrayToStr method in ID3.js JW8-359

--- src/utils/webvtt-parser.js
+++ src/utils/webvtt-parser.js
@@ -1,4 +1,5 @@
 import VTTParser from './vttparser';
+import { utf8ArrayToStr } from '../demux/id3'
 
 // String.prototype.startsWith is not supported in IE11
 const startsWith = function(inputString, searchString, position) {
@@ -54,43 +55,6 @@ const calculateOffset = function(vttCCs, cc, presentationTime) {
     }
 
     vttCCs.presentationOffset = presentationTime;
-};
-
-const utf8ArrayToStr = function(array, startingIndex) {
-    const len = array.length;
-    let c;
-    let char2;
-    let char3;
-    let out = '';
-    let i = startingIndex || 0;
-    while (i < len) {
-        c = array[i++];
-        // If the character is 3 (END_OF_TEXT) or 0 (NULL) then skip it
-        if (c === 0x00 || c === 0x03) {
-            continue;
-        }
-        switch (c >> 4) {
-            case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
-            // 0xxxxxxx
-                out += String.fromCharCode(c);
-                break;
-            case 12: case 13:
-            // 110x xxxx   10xx xxxx
-                char2 = array[i++];
-                out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
-                break;
-            case 14:
-                // 1110 xxxx  10xx xxxx  10xx xxxx
-                char2 = array[i++];
-                char3 = array[i++];
-                out += String.fromCharCode(((c & 0x0F) << 12) |
-                    ((char2 & 0x3F) << 6) |
-                    ((char3 & 0x3F) << 0));
-                break;
-            default:
-        }
-    }
-    return out;
 };
 
 const WebVTTParser = {
