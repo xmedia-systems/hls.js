1cd62c00b23f319be8bf6430be38f2b76b2c7ab9
John Bartos <jbartos@jwplayer.com>
2019-03-11 15:06:06 -0400

    Delete demo code

--- src/controller/level-controller.js
+++ src/controller/level-controller.js
@@ -396,8 +396,7 @@ export default class LevelController extends EventHandler {
     }
     // if current playlist is a live playlist, arm a timer to reload it
     if (details.live) {
-      // const reloadInterval = computeReloadInterval(curLevel.details, details, data.stats.trequest);
-      const reloadInterval = 1000;
+      const reloadInterval = computeReloadInterval(curLevel.details, details, data.stats.trequest);
       logger.log(`live playlist, reload in ${Math.round(reloadInterval)} ms`);
       this.timer = setTimeout(() => this.loadLevel(), reloadInterval);
     } else {
