d438f374cc280dc2ab67f973034b62725ff65a09
John Bartos <jbartos@jwplayer.com>
2019-07-12 20:08:38 -0400

    Improve alternate audio fragment loading, and general fragment finding behavior for live streams JW8-9842

--- src/demux/aacdemuxer.ts
+++ src/demux/aacdemuxer.ts
@@ -29,6 +29,9 @@ class AACDemuxer implements Demuxer {
   resetTimeStamp () {
   }
 
+  resetContiguity (): void {
+  }
+
   // Source for probe info - https://wiki.multimedia.cx/index.php?title=ADTS
   static probe (data) {
     if (!data) {
--- src/demux/mp3demuxer.ts
+++ src/demux/mp3demuxer.ts
@@ -22,6 +22,9 @@ class MP3Demuxer implements Demuxer {
   resetTimeStamp () {
   }
 
+  resetContiguity (): void {
+  }
+
   static probe (data) {
     // check if data contains ID3 timestamp and MPEG sync word
     let offset, length;
